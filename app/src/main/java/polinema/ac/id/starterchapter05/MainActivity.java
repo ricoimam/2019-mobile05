55ac6aecb24cc231f421bd17bcde799193a2526a
ricoimam <ricoimam14@gmail.com>
2019-11-04 05:47:23 +0700

    penambahan semua fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlerClickStaticFragment(View view) {
+        Intent i = new Intent(this, StaticActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/StaticActivity.java
@@ -0,0 +1,16 @@
+
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import polinema.ac.id.starterchapter05.R;
+
+import android.os.Bundle;
+
+public class StaticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static);
+    }
+}
